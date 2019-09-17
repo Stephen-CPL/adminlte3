a4ce924f030afe93994aa531470965fb3ec1a826
stephencheuk <stephen@cplhk.com>
2019-09-17 09:06:53 +0800

    init commit

---
+++ src/i18n/zh.js
@@ -0,0 +1,162 @@
+import englishMessages from 'ra-language-english';
+
+export default {
+    ...englishMessages,
+    pos: {
+        search: '搜索',
+        configuration: '配置',
+        language: '語言',
+        theme: {
+            name: '主題',
+            light: 'Light',
+            dark: 'Dark',
+        },
+        dashboard: {
+            monthly_revenue: 'Monthly Revenue',
+            new_orders: 'New Orders',
+            pending_reviews: 'Pending Reviews',
+            new_customers: 'New Customers',
+            pending_orders: 'Pending Orders',
+            order: {
+                items:
+                    'by %{customer_name}, one item |||| by %{customer_name}, %{nb_items} items',
+            },
+            welcome: {
+                title: 'Welcome to react-admin demo',
+                subtitle:
+                    "This is the admin of an imaginary poster shop. Feel free to explore and modify the data - it's local to your computer, and will reset each time you reload.",
+                aor_button: 'react-admin site',
+                demo_button: 'Source for this demo',
+            },
+        },
+        menu: {
+            sales: '銷售',
+            catalog: '目錄',
+            customers: '客户',
+        },
+    },
+    resources: {
+        customers: {
+            name: 'Customer |||| Customers',
+            fields: {
+                commands: 'Orders',
+                groups: '標籤',
+                last_seen_gte: 'Visited Since',
+                name: 'Name',
+                total_spent: 'Total spent',
+            },
+            tabs: {
+                identity: 'Identity',
+                address: 'Address',
+                orders: 'Orders',
+                reviews: 'Reviews',
+                stats: 'Stats',
+            },
+            page: {
+                delete: 'Delete Customer',
+            },
+        },
+        commands: {
+            name: 'Order |||| Orders',
+            title: 'Order %{reference}',
+            fields: {
+                basket: {
+                    delivery: 'Delivery',
+                    reference: 'Reference',
+                    quantity: 'Quantity',
+                    sum: 'Sum',
+                    tax_rate: 'Tax Rate',
+                    total: 'Total',
+                    unit_price: 'Unit Price',
+                },
+                customer_id: 'Customer',
+                date_gte: 'Passed Since',
+                date_lte: 'Passed Before',
+                total_gte: 'Min amount',
+                status: 'Status',
+                returned: 'Returned',
+            },
+        },
+        invoices: {
+            name: 'Invoice |||| Invoices',
+            fields: {
+                date: 'Invoice date',
+                customer_id: 'Customer',
+                command_id: 'Order',
+                date_gte: 'Passed Since',
+                date_lte: 'Passed Before',
+                total_gte: 'Min amount',
+                address: 'Address',
+            },
+        },
+        products: {
+            name: 'Poster |||| Posters',
+            fields: {
+                category_id: 'Category',
+                height_gte: 'Min height',
+                height_lte: 'Max height',
+                height: 'Height',
+                image: 'Image',
+                price: 'Price',
+                reference: 'Reference',
+                stock_lte: 'Low Stock',
+                stock: 'Stock',
+                thumbnail: 'Thumbnail',
+                width_gte: 'Min width',
+                width_lte: 'Max width',
+                width: 'Width',
+            },
+            tabs: {
+                image: 'Image',
+                details: 'Details',
+                description: 'Description',
+                reviews: 'Reviews',
+            },
+        },
+        categories: {
+            name: 'Category |||| Categories',
+            fields: {
+                products: 'Products',
+            },
+        },
+        reviews: {
+            name: 'Review |||| Reviews',
+            detail: 'Review detail',
+            fields: {
+                customer_id: 'Customer',
+                command_id: 'Order',
+                product_id: 'Product',
+                date_gte: 'Posted since',
+                date_lte: 'Posted before',
+                date: 'Date',
+                comment: 'Comment',
+                rating: 'Rating',
+            },
+            action: {
+                accept: 'Accept',
+                reject: 'Reject',
+            },
+            notification: {
+                approved_success: 'Review approved',
+                approved_error: 'Error: Review not approved',
+                rejected_success: 'Review rejected',
+                rejected_error: 'Error: Review not rejected',
+            },
+        },
+        segments: {
+            name: '標籤',
+            fields: {
+                customers: 'Customers',
+                name: 'Name',
+            },
+            data: {
+                compulsive: '急切',
+                collector: '收藏',
+                ordered_once: '單次訂購',
+                regular: '定期',
+                returns: '退回',
+                reviewer: '評論者',
+            },
+        },
+    },
+};
